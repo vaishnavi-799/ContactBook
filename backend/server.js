39a71a90ff41490acfee8cd62c6e9b53a9001aa0
vaishnavi-799 <vaishnavigurramkonda04@gmail.com>
2026-07-01 19:27:51 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const mongoose=require("mongoose");
 const cors=require("cors");
+require("dotenv").config();
 
 const authRoutes=require("./routes/auth");
 const contactRoutes=require("./routes/contact");
@@ -8,12 +9,15 @@ const contactRoutes=require("./routes/contact");
 const app=express();
 app.use(cors());
 app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect("mongodb://127.0.0.1:27017/contactbook")
+mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("MongoDB Connected"))
 .catch(err=>console.log(err));
 
 app.use("/api/auth",authRoutes);
 app.use("/api/contacts",contactRoutes);
 
-app.listen(5000,()=>console.log("Server running on port 5000"));
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
