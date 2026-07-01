c76a4d0324cf2dcbc65ef7e61717fd439b7f9496
vaishnavi-799 <vaishnavigurramkonda04@gmail.com>
2026-07-01 19:38:26 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,29 @@
-const express=require("express");
-const mongoose=require("mongoose");
-const cors=require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
-const authRoutes=require("./routes/auth");
-const contactRoutes=require("./routes/contact");
+const authRoutes = require("./routes/auth");
+const contactRoutes = require("./routes/contact");
 
-const app=express();
+const app = express();
+
 app.use(cors());
 app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDB Connected"))
-.catch(err=>console.log(err));
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
-app.use("/api/auth",authRoutes);
-app.use("/api/contacts",contactRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/contacts", contactRoutes);
+
+// 👇 Add this here
+app.get("/", (req, res) => {
+    res.send("ContactBook API is running 🚀");
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
